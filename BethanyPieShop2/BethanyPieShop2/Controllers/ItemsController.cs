ac63f19239452a6cdec4bb319ceb9a9b46faa2e5
Shanthi Kolakani <shanthi.kolakani@zensar.com>
2020-08-30 14:56:54 +0530

    Add new file to master

---
+++ BethanyPieShop2/BethanyPieShop2/Controllers/ItemsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace BethanyPieShop2.Controllers
+{
+    public class ItemsController : Controller
+    {
+        // GET: Items
+      
+        public ActionResult Index1()
+        {
+           
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            return View();
+        }
+        public ActionResult Index3()
+        {
+            return View();
+        }
+        public ActionResult Details() 
+        {
+            return View();
+        }
+    }
+}
